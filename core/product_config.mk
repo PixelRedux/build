9b9b9cf2b90d60e1b8100c1564d3d261c3e200c6
Arnav Gosain <gosain.arnav@gmail.com>
2014-10-14 15:44:35 -0400

    Inherit device makefile from vendor/pixel/products

--- core/product_config.mk
+++ core/product_config.mk
@@ -185,7 +185,7 @@ all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
   ifneq ($(PIXEL_BUILD),)
-    all_product_configs := $(shell ls device/*/$(PIXEL_BUILD)/cm.mk)
+    all_product_configs := $(shell ls vendor/pixel/products/$(PIXEL_BUILD).mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
